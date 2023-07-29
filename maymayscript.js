464bd9c15656ac26d4a55f7a5d1e9fb8cf84c033
harendrakr369 <harendrabaghel532@gmail.com>
2023-07-29 22:35:12 +0530

    commit;

--- maymayscript.js
+++ maymayscript.js
@@ -1,6 +1,6 @@
 function generateMeme() {
-    const topText = document.getElementById("topText").innerText;
-    const bottomText = document.getElementById("bottomText").innerText;
+    const topText = document.getElementById("topText").value;
+    const bottomText = document.getElementById("bottomText").value;
     const memeImage = document.getElementById("memeImage");
   
     const fileInput = document.getElementById("imageInput");
@@ -17,11 +17,7 @@ function generateMeme() {
       reader.readAsDataURL(file);
     } else {
       // No image selected, use a placeholder image
-      const imageUrl = "https://via.placeholder.com/400x300";
-      memeImage.src = imageUrl;
+      memeImage.src = "https://via.placeholder.com/400x300";
     }
-  
-    document.getElementById("topText").innerText = topText;
-    document.getElementById("bottomText").innerText = bottomText;
   }
   
