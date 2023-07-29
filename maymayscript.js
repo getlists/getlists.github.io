cceb2a61765beddadb48b6e343aac42c08611891
harendrakr369 <harendrabaghel532@gmail.com>
2023-07-29 22:27:01 +0530

    commit;

--- maymayscript.js
+++ maymayscript.js
@@ -3,10 +3,24 @@ function generateMeme() {
     const bottomText = document.getElementById("bottomText").innerText;
     const memeImage = document.getElementById("memeImage");
   
-    // Replace this URL with the actual image URL or use image upload functionality (requires backend)
-    const imageUrl = "https://via.placeholder.com/400x300";
+    const fileInput = document.getElementById("imageInput");
+    const file = fileInput.files[0];
   
-    memeImage.src = imageUrl;
+    if (file) {
+      const reader = new FileReader();
+  
+      reader.onload = function(event) {
+        const imageUrl = event.target.result;
+        memeImage.src = imageUrl;
+      };
+  
+      reader.readAsDataURL(file);
+    } else {
+      // No image selected, use a placeholder image
+      const imageUrl = "https://via.placeholder.com/400x300";
+      memeImage.src = imageUrl;
+    }
+  
     document.getElementById("topText").innerText = topText;
     document.getElementById("bottomText").innerText = bottomText;
   }
